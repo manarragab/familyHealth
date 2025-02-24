8448829f2ebf1a2f1dd0974460cfc36f2b9ede4d
manar ragab <manarragab670@gmail.com>
2025-02-24 18:56:45 +0200

    Pink Screens by manar

--- lib/features/calculation/presentation/dateCalc_screen.dart
+++ lib/features/calculation/presentation/dateCalc_screen.dart
@@ -1,5 +1,6 @@
 import 'package:abg/features/calculation/presentation/dueDate_screen.dart';
 import 'package:abg/features/calculation/presentation/widget/pinkContainer.dart';
+import 'package:abg/features/calculation/presentation/widget/whiteContainer.dart';
 import 'package:abg/localization/all_keys.dart';
 import 'package:abg/res/configuration/app_bar.dart';
 import 'package:abg/res/configuration/color.dart';
@@ -25,95 +26,17 @@ class DatecalcScreen extends StatelessWidget {
                   SizedBox(
                     height: 20,
                   ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 9),
-                    width: double.infinity,
-                    height: 71,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                    ),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          "assets/images/pregnant.png",
-                          width: 50,
-                          height: 50,
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Expected birth date",
-                              style: GoogleFonts.almarai(
-                                color: CustomColors.darkBlue2,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            Text(
-                              "22/06/2025",
-                              style: GoogleFonts.almarai(
-                                color: CustomColors.darkBlue2,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
+                  Whitecontainer(
+                      image: "assets/images/pregnant.png",
+                      title: "Expected birth date",
+                      subTitle: "22/06/2025"),
                   SizedBox(
                     height: 11,
                   ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 9),
-                    width: double.infinity,
-                    height: 71,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                    ),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          "assets/images/loveBook.png",
-                          width: 50,
-                          height: 50,
-                        ),
-                        SizedBox(
-                          width: 8,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Expected birth date",
-                              style: GoogleFonts.almarai(
-                                color: CustomColors.darkBlue2,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            Text(
-                              "22/06/2025",
-                              style: GoogleFonts.almarai(
-                                color: CustomColors.darkBlue2,
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
+                  Whitecontainer(
+                      image: "assets/images/loveBook.png",
+                      title: "Expected birth date",
+                      subTitle: "22/06/2025")
                 ],
               ),
             ),
