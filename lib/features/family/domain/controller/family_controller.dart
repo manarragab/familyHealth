9089b282f26ee65936c6534de6fcf056ec4ecc32
aymanKickStart <ayman.atef@kickstartinteractive.com>
2025-03-07 14:34:38 +0200

    add family feature

---
+++ lib/features/family/domain/controller/family_controller.dart
@@ -0,0 +1,4 @@
+import 'package:abg/data/const/export.dart';
+import 'package:abg/domain_data/custom_mixin/custom_state_mixin.dart';
+
+class FamilyController extends MainGetxController with CustomStateMixin {}
