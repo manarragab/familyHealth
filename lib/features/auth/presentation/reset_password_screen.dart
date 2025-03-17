4900ab04d63654c5cd752cfaf22c821c20544148
aymanKickStart <ayman.atef@kickstartinteractive.com>
2025-03-17 16:02:52 +0200

    fix password in reset password

--- lib/features/auth/presentation/reset_password_screen.dart
+++ lib/features/auth/presentation/reset_password_screen.dart
@@ -8,61 +8,66 @@ class ResetPasswordScreen extends GetView<AuthController> {
 
   final hidePassword = true.obs;
   final hideConfirmPassword = true.obs;
-
+final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar.appBarLogo(displayLogo: false),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.zero,
-            title: Text(
-              CustomTrans.resetPassword.tr,
-              style: TFonts.textTitleStyle(),
+      body: Form(
+        key: formKey,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              title: Text(
+                CustomTrans.resetPassword.tr,
+                style: TFonts.textTitleStyle(),
+              ),
+              subtitle: Text(CustomTrans.enterTheNewPassword.tr),
             ),
-            subtitle: Text(CustomTrans.enterTheNewPassword.tr),
-          ),
-          const SizedBox(height: 10),
-          Obx(() {
-            return CustomTextField.passwordTextField(
-              (value) => controller.postRegister.password = value,
-              isVisible: hidePassword.value,
-              changeVisible: () {
-                hidePassword(!hidePassword.value);
-              },
-            );
-          }),
-          const SizedBox(height: 10),
-          Obx(() {
-            return CustomTextField.passwordTextField(
-              (value) => null,
-              isVisible: hideConfirmPassword.value,
-              validator: (value) {
-                if (controller.postRegister.password != value) {
-                  return CustomTrans.wrongConfirmedPassword.tr;
-                }
-                return null;
-              },
-              changeVisible: () {
-                hideConfirmPassword(!hideConfirmPassword.value);
-              },
-              hint: CustomTrans.confirmPassword.tr,
+            const SizedBox(height: 10),
+            Obx(() {
+              return CustomTextField.passwordTextField(
+                (value) => controller.postRegister.password = value,
+                isVisible: hidePassword.value,
+                changeVisible: () {
+                  hidePassword(!hidePassword.value);
+                },
+              );
+            }),
+            const SizedBox(height: 10),
+            Obx(() {
+              return CustomTextField.passwordTextField(
+                (value) => null,
+                isVisible: hideConfirmPassword.value,
+                validator: (value) {
+                  if (controller.postRegister.password != value) {
+                    return CustomTrans.wrongConfirmedPassword.tr;
+                  }
+                  return null;
+                },
+                changeVisible: () {
+                  hideConfirmPassword(!hideConfirmPassword.value);
+                },
+                hint: CustomTrans.confirmPassword.tr,
 
-              //  controller:
-            );
-          }),
-          const SizedBox(height: 20),
-          LoadingOverLay(
-            showLoadingOnly: true,
-            child: MainButton(
-                title: CustomTrans.done.tr,
-                onPressed: () {
-                  controller.setPassword();
-                }),
-          )
-        ],
+                //  controller:
+              );
+            }),
+            const SizedBox(height: 20),
+            LoadingOverLay(
+              showLoadingOnly: true,
+              child: MainButton(
+                  title: CustomTrans.done.tr,
+                  onPressed: () {
+                    if(formKey.currentState!.validate()){
+                      controller.setPassword();
+                    }
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
